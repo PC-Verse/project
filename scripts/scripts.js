dbe69a62c0d1ff33adad11cdc17ffbbeaa1b1a84
BenYangSB <72826356+BenYangSB@users.noreply.github.com>
2020-11-15 15:15:14 -0800

    rectangles and other logic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,52 @@
+var boxContainer;
+var numRects;
+
+function initialize() {
+    //boxContainer = document.getElementById( "Box-Container" );
+    createRectangles() ;
+    // console.log( "Page Loaded! :)" );
+    /*  Add and remove HTML elements */
+}
+
+function getRandHeight(){
+    let randH = Math.floor(300 * Math.random(0));
+    return randH;
+}
+
+function getY(){
+    return parseInt(70/numRects).toString() + "vh";
+}
+
+
 function createRectangles() {
-    let NumRectangles = getElementById("amountOfRectangles");
+    boxContainer = document.getElementById( "Box-Container" );
+
+    
+    let rects = document.getElementsByClassName( "rectangle" );
+     
+    while ( rects.length != 0 ){
+        boxContainer.removeChild( rects[0] );
+
+    }
+
+
+
+    //let NumRectangles = getElementById("amountOfRectangles");
+    let N = document.getElementById("amountOfRectangles");
+    N = parseInt(N.value);
+    numRects = N;
+    //console.log(N);
+    for(let i =0 ; i<N; i++){
+        console.log(i);
+        let box = document.createElement( "div" );
+        box.className = "rectangle";
+        box.style.backgroundColor= 'blue';
+        let x =  getRandHeight();
+        let y = x.toString() + "px";
+        box.style.width = y;
+        box.style.height = getY();
+
+        boxContainer.appendChild( box );
+    }
+
 }
