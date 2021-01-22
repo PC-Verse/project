f34b6815ade532fc222d9ef31c857bbef6db84ea
Kevin Zhang <kevinzhang425@gmail.com>
2021-01-21 20:04:14 -0800

    Practice components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
+import Button from './components/Button'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <Greet name="Kevin">
+          Child of Greet
+        </Greet>
+        <Welcome/>
+
+        
+          {/* <br/> */}
+          <Button name="kevin"/>
+          {/* <br/> */}
+          <Button name="benjamin"/>
+
         <a
           className="App-link"
           href="https://reactjs.org"
