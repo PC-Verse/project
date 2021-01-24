4409296f97babf4cb7b87e433d97b598bee0d9ed
BenYangSB <72826356+BenYangSB@users.noreply.github.com>
2021-01-23 22:50:30 -0800

    posting mech

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 import Greet from './components/Greet'
 import Button from './components/Button'
 import Welcome from './components/Welcome'
 import NavBar from './components/NavBar'
+import AddPost from './components/AddPost'
 import Post from './components/Post'
 
 
 function App() {
+
+  // const AddedElement = () => <div><input placeholder='text box' /></div>
+  const [count, setCount] = useState(0)
+
   return (
     <div className="body">      
       {/* <header className="App-header"> */}
         <NavBar/>
-        <Post/>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+        { Array(count).fill(<Post title = "test posts" body = "Body is here"/>) }
+        <AddPost/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
+
+
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         
         {/* <Greet name="Kevin">
@@ -35,8 +53,3 @@ function App() {
           Learn React
         </a> */}
       {/* </header> */}
-    </div>
-  );
-}
-
-export default App;
---
+++ src/components/AddPost.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import '../App.css'
+
+class AddPost extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render = () => {
+
+        return(
+            <div class = "card">
+
+            <p>Your Post Here:</p>
+                <form class = "container" onSubmit={this.handleSubmit} >
+                    <div>
+                        <input type="text" id="fname" class="title" placeholder="Your title.."></input>
+                    </div>
+                    <div>
+                        <input type="text" value={this.state.value} onChange={this.handleChange} class="description" placeholder="Description..."></input>
+                    </div>
+                    <button class = "post">Post!</button>
+                </form>
+             {/* </div><input type="text" id="fname" name="firstname" placeholder="Your name.."> */}
+            </div>
+        )
+    }
+}
+
+
+
+export default AddPost
