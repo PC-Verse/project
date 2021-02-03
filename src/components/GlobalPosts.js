97fd0ed4bec58bea13b24ae8a0073e2d9064ba4f
Kevin Zhang <kevinzhang425@gmail.com>
2021-02-02 17:15:15 -0800

    Experimenting with displaying stuff from database

--- src/components/GlobalPosts.js
+++ src/components/GlobalPosts.js
@@ -4,6 +4,9 @@ import Post from './Post'
 class GlobalPosts extends Component {
     constructor() {
         super()
+        this.state = {
+            globalPosts: []
+        }
     }
 
     componentDidMount = () => {
@@ -20,20 +23,34 @@ class GlobalPosts extends Component {
             <div>
                 <div id="globalPostTitle">COMMUNITY</div>
                 {this.props.globalPosts.map((post) => {
-                        return post;
-                    })
+                    return post;
+                })
                 }
-                {/*The code down here doesn'tget shown on the website for some reason, but it is logging the correct stuff*/}
+                {/*The code down here doesn't get shown on the website for some reason, but it is logging the correct stuff. It logs on every rerender of the page, so the console will get filled up*/}
                 {this.props.database != null &&
-                    this.props.database.ref('globalPosts').on('value', (snap) => {
-                        let post = snap.val()[0];
-                        console.log(post)
-                        return <Post title={post.title} content={post.content} dateDay={post.dateDay} dateTime={post.dateTime} id={post.id} />
+                    this.props.database.ref('globalPosts').on('value',(posts) => {
+                        posts.forEach(post => {
+                            console.log(post.val())
+                            // let posts = this.state.globalPosts
+                            // posts.unshift(<Post title={post.title} content={post.content} dateDay={post.dateDay} dateTime={post.dateTime} id={post.id} imageList={[]} removePost={this.removePost} isGlobalPost={true} name={post.name} />)
+                            // this.setState({
+                            //     globalPosts: posts
+                            // })
+                        })
+                        // let post = posts.val()[0];
+                        // console.log(post)
                     })
                 }
-                {/* {this.props.dbGlobalPostsRef().map(post => (
-                    <Post title={post.title} content={post.content} dateDay={post.dateDay} dateTime={post.dateTime} id={post.id} name={post.name}/>
-                ))} */}
+                {/* {this.state.globalPosts.map((post) => {
+                    return post;
+                })
+                } */}
+
+                {/* } */}
+                {/* {this.props.database != null &&
+                    this.props.database.ref('globalPosts').map(post => (
+                        <Post title={post.title} content={post.content} dateDay={post.dateDay} dateTime={post.dateTime} id={post.id} name={post.name} />
+                    ))} */}
             </div>
         )
     }
