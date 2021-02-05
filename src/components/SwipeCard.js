c726e3f3dc448f95f57ee1784e08fde2a883f2a4
Kevin Zhang <kevinzhang425@gmail.com>
2021-02-04 22:31:53 -0800

    Fixed image stuff

--- src/components/SwipeCard.js
+++ src/components/SwipeCard.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
 import '../App.css'
-import Pic from '../images/RGB.png'
+// import Pic from '../images/RGB.png'
 
 class SwipeCard extends Component {
     constructor(props) {
         super(props)
+        // const pic = require(this.props.link)
+        // this.state = {
+        //     pic: pic
+        // }
     }
 
    
     render = () => {
         return (
             <div className = "swipeCard">
-             <img id="swipeIMG" src={
-                 window.location.origin + this.props.link
-             }></img>
+             <img id="swipeIMG"
+                src={window.location.origin + this.props.link}>
+            </img>
               <div className = "image">
                   <h></h>
                  
