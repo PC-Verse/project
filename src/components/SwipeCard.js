93ba28cfea6b2937b67d04c3cdab9120e1fa0eba
BenYangSB <72826356+BenYangSB@users.noreply.github.com>
2021-02-04 23:29:14 -0800

    btn

--- src/components/SwipeCard.js
+++ src/components/SwipeCard.js
@@ -28,8 +28,8 @@ class SwipeCard extends Component {
                                 <i class="fa fa-long-arrow-right arrow1" aria-hidden="true"></i>
                             </button>
                         </div>` */}
-                <button class ="btn1" onClick={this.props.swipeLeft} > left </button>
-                <button  class ="btn1" onClick={this.props.swipeRight} > right  </button>
+                <button class ="btn1" onClick={this.props.swipeLeft} > &lt;&lt;  </button>
+                <button  class ="btn1" onClick={this.props.swipeRight} > &#x3e; &#x3e;  </button>
               </div>
 
             </div>
