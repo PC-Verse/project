4efec8c39f263513760c6c93d7952705d455cb5e
Ben Yang <benjaminyang@ucsb.edu>
2021-02-26 22:00:53 -0800

    index on the pics

--- src/components/PicturesList.js
+++ src/components/PicturesList.js
@@ -33,11 +33,17 @@ class PicturesList extends Component {
     render = () => {
         console.log("HERE");
         return (
+            <div>
             <div id="picture-wheel">
                 <button onClick = {()=>{this.goNext(-1)}}>&#8592;</button>
                 <img src={this.props.images[this.state.index]['data_url']} className="picture" id="postPic"/>
                 <button onClick = {()=>{this.goNext(1)}}>&#8594;</button>
             </div>
+            <div>
+                <p>{this.state.index+1} / {this.props.images.length}</p>
+            </div>
+            </div>
+
         )
     }
 }
