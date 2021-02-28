243fab1d93aeab8e2c3ac65c9ca8cc5a4c7baef4
Kevin Zhang <kevinzhang425@gmail.com>
2021-02-27 23:08:34 -0800

    Changed class of postPic in pictureList so when u switch pic it is aniamted. Slight bug: if u hit next repeatedly with no pause, the setTimeout will set fade-in to false in the middle of an animation

--- src/components/PicturesList.js
+++ src/components/PicturesList.js
@@ -3,24 +3,35 @@ import React, { Component } from 'react'
 import '../App.css'
 import { CSSTransition } from 'react-transition-group'
 import { getNodeText } from '@testing-library/react'
+import ReactCSSTransitionGroup from 'react-transition-group'
 
 class PicturesList extends Component {
     constructor() {
         super()
         this.state = {
-            index: 0
+            index: 0,
+            fade_in : true
         }
-
+        this.setFadeInFalse()
     }
 
-
+    setFadeInFalse = () => {
+        setTimeout(() => {
+            this.setState({
+                fade_in: false
+            })
+        }, 1500)
+    }
     goNext = (a) => {
-
+        this.setState({
+            fade_in: false
+        })
         let newIndex = (this.state.index == 0 && a == -1 ? this.props.images.length - 1 : this.state.index + a) % this.props.images.length
         this.setState({
-            index: newIndex
+            index: newIndex,
+            fade_in : true
         })
-
+        this.setFadeInFalse()
     }
 
 
@@ -31,13 +42,23 @@ class PicturesList extends Component {
     }
 
     render = () => {
-        console.log("HERE");
+        // console.log("HERE");
+        let picClasses = ['picture', this.state.fade_in ? 'fade-in' : ''].join(' ');
         return (
             <div>
                 <div id="picture-wheel">
                     {this.props.images.length > 1 &&
                         <button id="leftTogglePicBtn" onClick={() => { this.goNext(-1) }}>&#8592;</button>}
-                    <img src={this.props.images[this.state.index]['data_url']} className="picture" id="postPic" alt="Post Picture"/>
+                    {/* <ReactCSSTransitionGroup
+                        transitionName="fade"
+                        transitionEnterTimeout={300}
+                        transitionLeaveTimeout={300}> */}
+                        <img src={this.props.images[this.state.index]['data_url']}
+                            key={this.props.images[this.state.index]['data_url']}
+                            className={picClasses}
+                            id="postPic"
+                            alt="Post Picture"/>
+                    {/* </ReactCSSTransitionGroup> */}
                     {this.props.images.length > 1 &&
                         <button id="rightTogglePicBtn" onClick={() => { this.goNext(1) }}>&#8594;</button>}
                 </div>
